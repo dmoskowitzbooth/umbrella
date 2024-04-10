ceb6c561dfc460e80d1e62b81a3eee013d096b7b
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-04-10 22:58:35 +0000

    1

---
+++ umbrella.rb
@@ -0,0 +1,7 @@
+require "http"
+require "json"
+print "Hello, there! Where are you located (or where do you want to check the need for an umbrella)?"
+location=gets
+pp "success"
+pirate_key=ENV["PIRATE"]
+
